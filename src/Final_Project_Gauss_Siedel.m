4b4fadec812c46a1c8d9c3421c25eb705de1fb11
Nazereneman <36425688+Nazereneman@users.noreply.github.com>
2018-05-08 23:44:15 -0500

    Final_Project_GS_7
    Corrected U algorithim to ensure U is multiplied by correct dx2 or dy2.

--- src/Final_Project_Gauss_Siedel.m
+++ src/Final_Project_Gauss_Siedel.m
@@ -8,6 +8,11 @@
 
 clc
 clear;
+close all;
+
+if exist('checkpt_GS.mat','file')     %If a checkpoint file exists, open it
+    load('checkpt_GS.mat')
+end
 
 %Given variables
 %lamda1=0.5;            %Given when using Helhmohtz, not needed for Poisson
@@ -19,8 +24,8 @@ by=2*Pi;                %Given
 v=0;                    %Given (du/dy @y=by = 0)
 
 %Initial Problem Setup 
-Lx=bx;                  %Length of x
-Ly=by;                  %Length of y
+Lx=bx-ax;                  %Length of x
+Ly=by-ay;                  %Length of y
 Nx=60;                  %Initial number of points in x
 Ny=Nx;                %Initial number of points in y
 deltax=Lx/(Nx+1);           %Step size in x
@@ -35,6 +40,9 @@ dy2=deltay*deltay;
 dx2=deltax*deltax;
 dx2dy2=deltax*deltax*deltay*deltay;
 denomin=-2*deltay*deltay-2*deltax*deltax;
+Pidy=Pi*deltay;
+gfbay=(gbay-fbay)/cons1;
+halfPi=Pi/2;
 
 %Initialize x and y to graph
 x=0:deltax:2*pi;
@@ -49,9 +57,9 @@ U1 = zeros(Nx+2,Ny+2);	% Preallocate solution matix with initial guess of ZERO
 
 %Boundary Conditions
 for j = 1:Ny+2                      
-    U1(1,j)=(by-deltay*(j-1))*(by-deltay*(j-1))*cos(Pi*deltay*(j-1)/by);    %u(x=ax,y)=fb(y) boundary condition
-    U1(Nx+2,j)=(deltay*(j-1))*(by-deltay*(j-1))*(by-deltay*(j-1));          %u(x=bx,y)=gb(y) boundary condition
-    U1(j,1)=fbay+(deltax*(j-1)-ax)/cons1*(gbay-fbay);                       %u(x,y=ay)  boundary condition
+    U1(1,j)=(by-deltay*(j-1))*(by-deltay*(j-1))*cos(Pidy*(j-1)/by);     %u(x=ax,y)=fb(y) boundary condition
+    U1(Nx+2,j)=(deltay*(j-1))*(by-deltay*(j-1))*(by-deltay*(j-1));      %u(x=bx,y)=gb(y) boundary condition
+    U1(j,1)=fbay+(deltax*(j-1)-ax)*gfbay;                               %u(x,y=ay)  boundary condition
 end
 
 %Error
@@ -64,7 +72,7 @@ F1=zeros(Nx+2,Ny+2);                %Preallocate F1
 
 for i=1:Nx+2
     for j=1:Ny+2
-        F1(i,j)=cos(Pi/2*(2*(deltax*(i-1)-ax)/(bx-ax)+1))*sin(Pi*(deltay*(j-1)-ay)/(by-ay));
+        F1(i,j)=cos(halfPi*(2*(deltax*(i-1)-ax)/(Lx)+1))*sin(Pi*(deltay*(j-1)-ay)/(Ly));
     end
 end
 
@@ -76,17 +84,37 @@ while err1>tol
             U1(i,j)=(dx2dy2*F1(i,j)-dy2*U1(i-1,j)-dy2*U1(i+1,j)-dx2*U1(i,j-1)-dx2*U1(i,j+1))/(denomin);
         end
         %Neumann Condition
-        U1(i,Ny+2)=(dx2dy2*F1(i,Ny+2)-dy2*U1(i-1,Ny+2)-dy2*U1(i+1,Ny+1)-dx2*U1(i,Ny+1)-dx2*U1(i,Ny+1))/(denomin);
+        U1(i,Ny+2)=(dx2dy2*F1(i,Ny+2)-dy2*U1(i-1,Ny+2)-dy2*U1(i+1,Ny+2)-dx2*U1(i,Ny+1)-dx2*U1(i,Ny+1))/(denomin);
     end
-    err1=max(max(abs(U1-Uprev1)./abs(U1)));
+    err1=max(max(abs(Uprev1-U1)./abs(Uprev1)));
     Uprev1=U1;
     iter1=iter1+1;
+    if mod(iter1,1500)==0                               %Save checkpoint file every 1500 iterations
+        save('checkpt_GS.mat');                   %Saving the file
+    end                                                 %Ending if loop
 end
 
+U1T=transpose(U1);      %Transpose the matrix so x and y axes are correct
+
 figure()
-surf(x,y,U1);
-%%
-% %Part 2
+h1=surf(x,y,U1T);
+xlabel('x')
+ylabel('y')
+set(h1,'linestyle','none')
+zlabel('U values');
+title('Surface Plot for GS (Nx=Ny=60)');
+colorbar;
+
+figure()
+contour(x,y,U1T);
+xlabel('x')
+ylabel('y')
+set(h1,'linestyle','none')
+zlabel('U values');
+title('Contour Plot for GS (Nx=Ny=60)');
+colorbar;
+
+%% Part 2
 %Boundary Conditions
 ucoef2=0.0;
 U2 = zeros(Nx+2,Ny+2);
@@ -102,14 +130,37 @@ Uprev2 = U2;
 while err2>tol
     for i=2:Ny+1
         for j=2:Nx+1
-            U2(i,j)=(dx2dy2*F2(i,j)-dy2*U2(i-1,j)-dy2*U2(i+1,j)-dx2*U2(i,j-1)-dy2*U2(i,j+1))/(denomin);
+            U2(i,j)=(dx2dy2*F2(i,j)-dy2*U2(i-1,j)-dy2*U2(i+1,j)-dx2*U2(i,j-1)-dx2*U2(i,j+1))/(denomin);
         end
-        U2(i,Ny+2)=(dx2dy2*F2(i,Ny+2)-dy2*U2(i-1,Ny+2)-dy2*U2(i+1,Ny+2)-dx2*U2(i,Ny+1)-dy2*U2(i,Ny+1))/(denomin);
+        U2(i,Ny+2)=(dx2dy2*F2(i,Ny+2)-dy2*U2(i-1,Ny+2)-dy2*U2(i+1,Ny+2)-dx2*U2(i,Ny+1)-dx2*U2(i,Ny+1))/(denomin);
     end
-    err2=max(max(abs(U2-Uprev2)./abs(U2)));
+    err2=max(max(abs(Uprev2-U2)./abs(Uprev2)));
     Uprev2=U2;
     iter2=iter2+1;
+    if mod(iter1,1500)==0                               %Save checkpoint file every 1500 iterations
+        save('checkpt_GS.mat');                   %Saving the file
+    end                                                 %Ending if loop
+
 end
 
+U2T=transpose(U2);      %Transpose the matrix so x and y axes are correct
+
 figure()
-surf(x,y,U2);
+h2=surf(x,y,U2T);
+xlabel('x')
+ylabel('y')
+set(h2,'linestyle','none')
+zlabel('U values');
+title('Surface Plot for GS (F=0, Nx=Ny=60)');
+colorbar;
+
+figure()
+contour(x,y,U2T);
+xlabel('x')
+ylabel('y')
+set(h2,'linestyle','none')
+zlabel('U values');
+title('Contour Plot for GS (F=0, Nx=Ny=60)');
+colorbar;
+%%
+delete('checkpt_GS.mat');             %Delete checkpoint file once complete
